0632170d51d49a1107442790142c93b89a473ec4
Paul Adaimi <adaimi.paul@gmail.com>
2025-01-08 14:58:12 +0200

    done Projects Design

--- components/ProjectsPage/VSCode.jsx
+++ components/ProjectsPage/VSCode.jsx
@@ -1,6 +1,5 @@
 import styles from "./VSCode.module.css";
 import ProjectsList from "./ProjectsList";
-import { useState } from "react";
 
 export default function VSCode({ projects, selectedProject, handleTabClick }) {
   return (
@@ -19,7 +18,84 @@ export default function VSCode({ projects, selectedProject, handleTabClick }) {
             handleTabClick={handleTabClick}
             projects={projects}
           />
-          <div className={styles.projectInfo}></div>
+          <div className={styles.projectInfo}>
+            <p className={styles.text}>
+              <span className={styles.code}>{"<Type> "}</span>
+              {selectedProject.type === "phone"
+                ? "Mobile Application"
+                : "Web Application"}
+              <span className={styles.code}>{" </Type>"}</span>
+            </p>
+            <p className={styles.text}>
+              <span className={styles.code}>{"<Year> "}</span>
+              {selectedProject.year}
+              <span className={styles.code}>{" </Year>"}</span>
+            </p>
+            {selectedProject.productionLinks && (
+              <p className={styles.text}>
+                <span className={styles.code}>{"<Production Links>"}</span>
+                <br />
+                {Object.keys(selectedProject.productionLinks).map(
+                  (linkKey, index) => (
+                    <>
+                      <span
+                        style={{ marginLeft: "20px" }}
+                        className={styles.code}
+                      >
+                        {"<" + linkKey + "> "}
+                      </span>
+                      <a
+                        href={selectedProject.productionLinks[linkKey]}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{
+                          textDecoration: "underline",
+                        }}
+                      >
+                        {selectedProject.productionLinks[linkKey]}
+                      </a>
+                      <span className={styles.code}>
+                        {" </" + linkKey + ">"}
+                      </span>
+                      <br />
+                    </>
+                  )
+                )}
+                <span className={styles.code}>{" </Production Links>"}</span>
+              </p>
+            )}
+
+            <p className={styles.text}>
+              <span className={styles.code}>{"<Technologies>"}</span>
+              <br />
+              {selectedProject.technologies.map((tech, index) => (
+                <>
+                  <span style={{ marginLeft: "20px" }} className={styles.code}>
+                    {"<Tech-" + (index + 1) + "> "}
+                  </span>
+                  {tech}
+                  <span className={styles.code}>
+                    {" </Tech-" + (index + 1) + ">"}
+                  </span>
+                  <br />
+                </>
+              ))}
+              <span className={styles.code}>{" </Technologies>"}</span>
+            </p>
+            <div className={styles.descriptionContainer}>
+              <p className={styles.text + " " + styles.code}>
+                {"<Description> "}
+              </p>
+              <div className={styles.description + " " + styles.text}>
+                <p className={styles.descriptionText}>
+                  {selectedProject.description}
+                </p>
+              </div>
+              <p className={styles.text + " " + styles.code}>
+                {"</Description> "}
+              </p>
+            </div>
+          </div>
           <ProjectsList
             prefix={"</"}
             selectedProject={selectedProject}
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,16 +6,41 @@ export default function Projects() {
     () => [
       {
         title: "Bleumer",
-        description: "This is the description for Project 1",
+        year: "2024",
+        technologies: ["Expo", "React Native", "Firebase", "Javascript"],
+        productionLinks: {
+          apple: "https://apps.apple.com/tr/app/bleumer/id6738310999?uo=2",
+          android: "coming soon",
+        },
+        description: (
+          <>
+            This mobile app is a salmon delivery platform designed for Bleumer
+            ME, a prominent Canadian brand in the salmon industry that expanded
+            to Lebanon in January 2015. <br />
+            <br />
+            Users can browse and shop for premium salmon products immediately
+            after downloading the app. To place an order, signing in is
+            required. Authentication is verified through WhatsApp using Twilio,
+            a cloud communications platform. Users receive real-time
+            notifications for any updates to their order status. <br />
+            <br />
+            The app is available on both the App Store and Google Play. Download
+            it using the links provided in the production links section.
+          </>
+        ),
         type: "phone",
       },
       {
         title: "Book Me",
+        year: "2018",
+        technologies: ["React", "Node.js", "Express", "Mongo", "Javascript"],
         description: "This is the description for Project 2",
         type: "web",
       },
       {
         title: "Air Hockey",
+        year: "2017",
+        technologies: ["Android", "Java"],
         description: "This is the description for Project 3",
         type: "phone",
       },
