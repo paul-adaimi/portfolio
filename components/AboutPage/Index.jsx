fef1921623ecc9f00650058b01e7294f863ded10
Paul Adaimi <adaimi.paul@gmail.com>
2025-01-10 18:11:18 +0200

    animation about me

--- components/AboutPage/Index.jsx
+++ components/AboutPage/Index.jsx
@@ -2,12 +2,19 @@ import styles from "./Index.module.css";
 import Accordion from "./Accordion";
 import AnimatedTextCoordinated from "../Global/AnimatedTextCoordinated";
 import Image from "next/image";
+import { useAnimatedTitle } from "../Global/AnimatedTitleProvider";
 
 export default function Index({ data }) {
+  const { shouldAnimateOut } = useAnimatedTitle();
+
   return (
     <div className={styles.main}>
       <div className={styles.content}>
-        <div className={styles.contentSide}>
+        <div
+          className={`${styles.contentSide} ${styles.contentSideLeft} ${
+            shouldAnimateOut && styles.animateOut
+          }`}
+        >
           <p className={styles.text}>
             {"<"}
             <AnimatedTextCoordinated typingSpeed={50} id={0}>
@@ -23,10 +30,16 @@ export default function Index({ data }) {
             alt="Paul Adaimi"
             width={400}
             height={400}
-            className={styles.imagePaul}
+            className={`${styles.imagePaul} ${
+              shouldAnimateOut && styles.animateOut
+            }`}
           />
         </div>
-        <div className={styles.contentSide}>
+        <div
+          className={`${styles.contentSide} ${styles.contentSideRight} ${
+            shouldAnimateOut && styles.animateOut
+          }`}
+        >
           <p className={styles.text}>
             {"<"}
             <AnimatedTextCoordinated typingSpeed={50} id={1}>
