210d83f545354bf5883ca5745593c36114f997d2
Paul Adaimi <adaimi.paul@gmail.com>
2025-01-13 03:39:54 +0200

    route transition fix part 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,28 @@
 import "../styles/global.css";
 import Layout from "../components/Layout";
-import Router from "next/router";
+
+// All imports here are a temporary fix for the animation out in production
+import "../components/HomePage/AnimatedTextScroll.module.css";
+import "../components/HomePage/Intro.module.css";
+import "../components/HomePage/PaulCoded.module.css";
+import "../components/HomePage/Index.module.css";
+
+import "../components/ProjectsPage/Index.module.css";
+import "../components/ProjectsPage/VSCode.module.css";
+import "../components/ProjectsPage/ProjectsList.module.css";
+import "../components/ProjectsPage/Cards/CardList.module.css";
+
+import "../components/AboutPage/Accordion.module.css";
+import "../components/AboutPage/AccordionItem.module.css";
+import "../components/AboutPage/Index.module.css";
+import "../components/AboutPage/SubSection.module.css";
+
+import "../components/Global/AnimatedTextCoordinated.module.css";
+import "../components/Global/AnimatedTextIndependant.module.css";
+import "../components/Global/AnimatedTitle.module.css";
+import "../components/Global/Laptop.module.css";
+import "../components/Global/Phone.module.css";
 
 export default function App({ Component, pageProps }) {
   return <Layout>{<Component {...pageProps} />}</Layout>;
 }
-
-const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
-
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute("media");
-    });
-  };
-  tempFix();
-};
-
-Router.events.on("routeChangeStart", routeChange);
-Router.events.on("routeChangeComplete", routeChange);
-Router.events.on("routeChangeStart", routeChange);
